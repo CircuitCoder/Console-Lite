742eabdac6020c2243f17ccbc65fd6f7ef2babce
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-08-21 17:43:38 +0800

    Added checking for vote rounds

--- controller/views/vote/vote.js
+++ controller/views/vote/vote.js
@@ -14,6 +14,8 @@ const VoteView = Vue.extend({
     targetVoter: { },
     targetVote: 0,
 
+    _overrideSetVote: false,
+
     VOTE_ROWS: [
       { code: 0, text: '过 / 未投票' },
       { code: 1, text: '赞成' },
@@ -75,6 +77,9 @@ const VoteView = Vue.extend({
     start() {
       if(this.vote.status.running) return;
 
+      if(this.vote.status.iteration === this.vote.rounds)
+        if(!confirm('按照预定轮数，投票已经结束，是否开始下一轮?')) return;
+
       this.$dispatch('iterate-vote', this.vote.id, {
         iteration: this.vote.status.iteration + 1,
         running: true
@@ -83,6 +88,10 @@ const VoteView = Vue.extend({
 
     stop() {
       if(!this.vote.status.running) return;
+
+      if(this.vote.status.iteration === this.vote.rounds)
+        if(this.vote.matrix.some(e => e.vote === 0))
+          if(!confirm('这是最后一轮投票了，还有投票为过的席位，是否结束这轮投票?')) return;
 
       this.$dispatch('iterate-vote', this.vote.id, {
         iteration: this.vote.status.iteration,
@@ -101,15 +110,23 @@ const VoteView = Vue.extend({
     },
 
     performManipulation() {
-      if(this.targetVote === this.targetVoter.vote) return; // Unchanged
 
-      this.$dispatch('update-vote', this.vote.id, this.targetVoter.originalId, this.targetVote);
+      if(!this._overrideSetVote
+        && this.targetVote === 0
+        && this.vote.status.iteration === this.vote.rounds) {
+          if(!confirm('这是最后一轮投票了，是否将投票结果设置为过?')) return;
+          else this._overrideSetVote = true;
+        }
+
+      if(this.targetVote !== this.targetVoter.vote) // Changed
+        this.$dispatch('update-vote', this.vote.id, this.targetVoter.originalId, this.targetVote);
+
       this.manipulateFlag = false;
     },
 
     cast() {
       this.$dispatch('project-vote', this.vote);
-    }
+    },
   },
 
   computed: {
