16bb3fd089d9866f32427c81641df7ceac00842b
Liu Xiaoyi <circuitcoder0@gmail.com>
2016-09-03 17:05:13 +0800

    Using travis, try 1

---
+++ bin/deploy-travis.js
@@ -0,0 +1,67 @@
+/* eslint-disable new-cap */
+const pack = require('./pack');
+
+const assert = require('assert');
+const fs = require('fs');
+const fstream = require('fstream');
+const minio = require('minio');
+const os = require('os');
+const path = require('path');
+const process = require('process');
+const rimraf = require('rimraf');
+const tar = require('tar');
+const zlib = require('zlib');
+
+let tag;
+
+if(process.env.TRAVIS_TAG && /^v\d+\.\d+\.\d+/.test(process.env.TRAVIS_TAG))
+  tag = process.env.TRAVIS_TAG;
+else if(process.env.TEST_UPLOAD)
+  tag = `c${process.env.TRAVIS_COMMIT}`;
+else
+  process.exit(0);
+
+const gzipOpt = {
+  memLevel: 9,
+  level: 9,
+};
+
+const basedir = path.dirname(__dirname);
+const targetdir = path.join(basedir, 'Console Lite');
+
+new Promise((resolve, reject) => pack((err, paths) => err ? reject(err) : resolve(paths)))
+  .then(paths => new Promise((resolve, reject) => {
+    assert(paths.length === 1);
+    const p = paths[0];
+
+    rimraf(targetdir);
+    fs.renameSync(p, targetdir);
+    const fname = `Console-Lite-${tag}-${os.platform()}-${os.arch()}.tar.gz`;
+
+    console.log(`Packing: ${fname}`);
+
+    fstream.Reader({
+      path: targetdir,
+      type: 'Directory',
+    })
+    .pipe(tar.Pack())
+    .pipe(zlib.createGzip(gzipOpt))
+    .pipe(fs.createWriteStream(path.join(basedir, fname)))
+    .on('end', () => resolve([ path.join(basedir, fname) ]))
+    .on('error', reject);
+  })).then(artifacts => {
+    const mc = new minio({
+      endPoint: 'store.bjmun.org',
+      secure: true,
+      accessKey: process.env.MINIO_ACCESS_KEY,
+      secretKey: process.env.MINIO_SECRET_KEY,
+    });
+
+    return Promise.all(artifacts.map(artifact => new Promise((resolve, reject) => {
+      mc.fPutObject('console-lite', artifact.split(/\./)[0], artifact, 'application/tar+gzip',
+                    (err, etag) => err ? reject(err) : resolve([artifact, etag]));
+    })));
+  })).catch(e => {
+    console.error(e.stack);
+    process.exit(1);
+  });
--- bin/pack.js
+++ bin/pack.js
@@ -2,29 +2,46 @@ const packager = require('electron-packager');
 const process = require('process');
 const path = require('path');
 
-console.log(`Building package for ${process.platform} - ${process.arch}.`);
-console.log('Please ensure that native dependecies are built using correct ABI version');
+function pack(cb, silent) {
+  if(!silent) {
+    console.log(`Building package for ${process.platform} - ${process.arch}.`);
+    console.log('Please ensure that native dependecies are built using correct ABI version');
+  }
 
-const opt = {
-  arch: process.arch,
-  platform: process.platform,
-  dir: path.join(__dirname, '..'),
-  ignore: [
-    /^\/server\/.*\.db($|\/)/,
-    /^\/server\/.*\.files($|\/)/,
-  ], // Ignores databases and files
-  tmpdir: false,
-  icon: path.join(__dirname, '../images/icon'),
-};
-
-if(process.env.ELECTRON_MIRROR)
-  opt.download = {
-    mirror: process.env.ELECTRON_MIRROR,
+  const opt = {
+    arch: process.arch,
+    platform: process.platform,
+    out: path.dirname(__dirname),
+    dir: path.dirname(__dirname),
+    prune: true,
+    ignore: [
+      /^\/server\/.*\.db($|\/)/,
+      /^\/server\/.*\.files($|\/)/,
+    ], // Ignores databases and files
+    tmpdir: false,
+    icon: path.join(__dirname, '../images/icon'),
   };
 
-packager(opt, (err, paths) => {
-  if(err) {
-    console.error('Packager failed:');
-    console.error(err.stack);
-  } else console.log(`Package outputted to: ${paths}`);
-});
+  if(process.env.ELECTRON_MIRROR)
+    opt.download = {
+      mirror: process.env.ELECTRON_MIRROR,
+    };
+
+  packager(opt, (err, paths) => {
+    if(err) {
+      if(!silent) {
+        console.error('Packager failed:');
+        console.error(err.stack);
+      }
+      if(cb) cb(err);
+    } else {
+      if(!silent)
+        console.log(`Package outputted to: ${paths}`);
+      if(cb) cb(null, paths);
+    }
+  });
+}
+
+if(require.main === module) pack();
+
+module.exports = pack;
